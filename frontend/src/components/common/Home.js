65426c4d62859c60348075a007ab07e5d29405b4
Nicolas-Dolan <nicolas.dolan@ymail.com>
2020-03-19 00:13:45 +0000

    improved home page

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import logo from './../../assets/Pokemon-Logo.png'
 const Home = () => (
   <section className="hero is-fullheight-with-navbar">
-    <div className="hero-body">
-      <div className="container frontPage">
-        <p className="title is-1 has-text-centered">Pokemon Unlimited</p>
+    <div className="hero-body frontPage centerIt2" id="animate-area">
+      <div className="">
+        <img src={logo}/>
+        <p className="title is-1 has-text-centered mainHeader">Unlimited</p>
       </div>
     </div>
   </section>
