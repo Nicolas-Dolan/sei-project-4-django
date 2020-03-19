6017f9db241be62dec980f13fab31cfe1c9517a5
Nicolas-Dolan <nicolas.dolan@ymail.com>
2020-03-19 17:17:52 +0000

    heroku update

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -4,7 +4,7 @@ const Home = () => (
   <section className="hero is-fullheight-with-navbar">
     <div className="hero-body frontPage centerIt2" id="animate-area">
       <div className="">
-        <img src={logo}/>
+        <img src={logo} />
         <p className="title is-1 has-text-centered mainHeader">Unlimited</p>
       </div>
     </div>
